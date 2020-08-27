384ce8d9fdc2046d6b1788fb3cc0975c6ff8b6cc
denimcouch <alexmata92@ymail.com>
2020-08-27 16:32:54 -0500

    created BankAccount class with instance methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    @@all = []
+
+    def initialize(name, balance= 1000)#, status= "open")
+        @name = name
+        @balance = balance
+        @status = "open"
+        self.class.all << self
+    end 
+    def self.all
+        @@all
+    end
+
+    def deposit(amount)
+        self.balance = self.balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0 
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
